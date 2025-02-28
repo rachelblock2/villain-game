59946f96a49817330f1fdfd5d9945230d629c1b9
rachelblock2 <36903263+rachelblock2@users.noreply.github.com>
2025-02-27 23:51:10 -0700

    icons designed, basic boards, working on implementing carousel

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,26 +1,61 @@
 // import { NavLink, Outlet } from "react-router-dom";
 
 export default function Board({
-  profile,
+  className,
+  profile
 }: {
+  className: string,
   profile: {
     name: string;
-    boardImages: { imageURL: string; imageAlt: string }[];
+    boardImages: {
+      imageURL: string;
+      imageAlt: string;
+      actions: { actionType: string; icon: string; amount?: string }[];
+    }[];
   };
 }) {
   return (
-    <div className="board_grid">
-      <p className="profile_name">{profile.name}</p>
+    <div className={`board_grid ${className}`}>
+      <div className="objective">
+        <div className="test">
+          <img
+            className="objective_img"
+            src="images/objectives/prince-john.jpg"
+            alt="Prince John"
+          />
+        </div>
+      </div>
+      {/* <p className="profile_name">{profile.name}</p> */}
       {profile.boardImages.map((image, index) => (
         <div className="board_section">
           <div className="board_background">
-            <img
-              key={index}
-              className="board_background_img"
-              src={image.imageURL}
-              alt={image.imageAlt}
-            />
+            <div className="top_actions"></div>
+            <div className="image_icons">
+              <img
+                key={index}
+                className="board_background_img"
+                src={image.imageURL}
+                alt={image.imageAlt}
+              />
+              <div
+                className={`icons ${
+                  image.actions.length === 3 ? "three_actions" : ""
+                }`}
+              >
+                {image.actions.map((action, actionIndex) => (
+                  <div>
+                    <img
+                      key={actionIndex}
+                      src={action.icon}
+                      alt={action.icon}
+                    />
+                    {action.amount !== undefined && <p>{action.amount}</p>}
+                  </div>
+                ))}
+              </div>
+            </div>
             <p className="location_text">{image.imageAlt}</p>
+            <div className="bottom_actions"></div>
           </div>
         </div>
       ))}
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Board from "./Board";
 
 interface MenuProps {
@@ -11,68 +12,195 @@ export default function Menu({
   beginGameSetup,
   gameSetUp,
 }: MenuProps) {
-  // const profiles = [1, 2, 3, 4, 5];
-  const profiles = [
+  const [profiles, setProfiles] = useState([
     {
+      id: 0,
       name: "Maleficent",
       boardImages: [
         {
           imageURL: "images/background_art/Forbidden_Mountain.PNG",
           imageAlt: "FORBIDDEN MOUNTAINS",
+          actions: [
+            {
+              actionType: "Gain Power",
+              icon: "images/gain_power.svg",
+              amount: "1",
+            },
+            { actionType: "Discard Cards", icon: "images/discard_cards.svg" },
+            { actionType: "Play a Card", icon: "images/play_a_card.svg" },
+            { actionType: "Fate", icon: "images/fate.svg" },
+          ],
         },
         {
           imageURL: "images/background_art/Forbidden_Mountain.PNG",
           imageAlt: "BRIAR ROSE'S COTTAGE",
+          actions: [
+            {
+              actionType: "Gain Power",
+              icon: "images/gain_power.svg",
+              amount: "1",
+            },
+            { actionType: "Discard Cards", icon: "images/discard_cards.svg" },
+            { actionType: "Play a Card", icon: "images/play_a_card.svg" },
+            { actionType: "Fate", icon: "images/fate.svg" },
+          ],
         },
         {
           imageURL: "images/background_art/Forbidden_Mountain.PNG",
           imageAlt: "THE FOREST",
+          actions: [
+            {
+              actionType: "Gain Power",
+              icon: "images/gain_power.svg",
+              amount: "1",
+            },
+            { actionType: "Discard Cards", icon: "images/discard_cards.svg" },
+            { actionType: "Play a Card", icon: "images/play_a_card.svg" },
+            { actionType: "Fate", icon: "images/fate.svg" },
+          ],
         },
         {
           imageURL: "images/background_art/Forbidden_Mountain.PNG",
           imageAlt: "KING STEFAN'S CASTLE",
+          actions: [
+            {
+              actionType: "Gain Power",
+              icon: "images/gain_power.svg",
+              amount: "1",
+            },
+            { actionType: "Discard Cards", icon: "images/discard_cards.svg" },
+            { actionType: "Play a Card", icon: "images/play_a_card.svg" },
+            { actionType: "Fate", icon: "images/fate.svg" },
+          ],
         },
       ],
     },
     {
+      id: 1,
       name: "Prince John",
       boardImages: [
         {
           imageURL: "images/background_art/Forbidden_Mountain.PNG",
           imageAlt: "SHERWOOD FOREST",
+          actions: [
+            {
+              actionType: "Gain Power",
+              icon: "images/gain_power.svg",
+              amount: "1",
+            },
+            { actionType: "Discard Cards", icon: "images/discard_cards.svg" },
+            { actionType: "Play a Card", icon: "images/play_a_card.svg" },
+            { actionType: "Fate", icon: "images/fate.svg" },
+          ],
         },
         {
           imageURL: "images/background_art/Forbidden_Mountain.PNG",
           imageAlt: "FRIAR TUCK'S CHURCH",
+          actions: [
+            {
+              actionType: "Gain Power",
+              icon: "images/gain_power.svg",
+              amount: "2",
+            },
+            { actionType: "Play a Card", icon: "images/play_a_card.svg" },
+            { actionType: "Play a Card", icon: "images/play_a_card.svg" },
+            {
+              actionType: "Move an Item or Ally",
+              icon: "images/move_an_item_or_an_ally.svg",
+            },
+          ],
         },
         {
           imageURL: "images/background_art/Forbidden_Mountain.PNG",
           imageAlt: "NOTTINGHAM",
+          actions: [
+            { actionType: "Fate", icon: "images/fate.svg" },
+            {
+              actionType: "Gain Power",
+              icon: "images/gain_power.svg",
+              amount: "1",
+            },
+            { actionType: "Vanquish", icon: "images/vanquish.svg" },
+            { actionType: "Play a Card", icon: "images/play_a_card.svg" },
+          ],
         },
         {
           imageURL: "images/background_art/Forbidden_Mountain.PNG",
           imageAlt: "THE JAIL",
+          actions: [
+            {
+              actionType: "Gain Power",
+              icon: "images/gain_power.svg",
+              amount: "3",
+            },
+            { actionType: "Play a Card", icon: "images/play_a_card.svg" },
+            { actionType: "Discard Cards", icon: "images/discard_cards.svg" },
+          ],
         },
       ],
     },
     {
+      id: 2,
       name: "Captain Hook",
       boardImages: [
         {
           imageURL: "images/background_art/Forbidden_Mountain.PNG",
           imageAlt: "SHERWOOD FOREST",
+          actions: [
+            {
+              actionType: "Gain Power",
+              icon: "images/gain_power.svg",
+              amount: "1",
+            },
+            { actionType: "Discard Cards", icon: "images/discard_cards.svg" },
+            { actionType: "Play a Card", icon: "images/play_a_card.svg" },
+            { actionType: "Fate", icon: "images/fate.svg" },
+          ],
         },
         {
           imageURL: "images/background_art/Forbidden_Mountain.PNG",
           imageAlt: "FRIAR TUCK'S CHURCH",
+          actions: [
+            {
+              actionType: "Gain Power",
+              icon: "images/gain_power.svg",
+              amount: "2",
+            },
+            { actionType: "Play a Card", icon: "images/play_a_card.svg" },
+            { actionType: "Play a Card", icon: "images/play_a_card.svg" },
+            {
+              actionType: "Move an Item or Ally",
+              icon: "images/move_an_item_or_an_ally.svg",
+            },
+          ],
         },
         {
           imageURL: "images/background_art/Forbidden_Mountain.PNG",
           imageAlt: "NOTTINGHAM",
+          actions: [
+            { actionType: "Fate", icon: "images/fate.svg" },
+            {
+              actionType: "Gain Power",
+              icon: "images/gain_power.svg",
+              amount: "1",
+            },
+            { actionType: "Vanquish", icon: "images/vanquish.svg" },
+            { actionType: "Play a Card", icon: "images/play_a_card.svg" },
+          ],
         },
         {
           imageURL: "images/background_art/Forbidden_Mountain.PNG",
           imageAlt: "THE JAIL",
+          actions: [
+            { actionType: "Fate", icon: "images/fate.svg" },
+            {
+              actionType: "Gain Power",
+              icon: "images/gain_power.svg",
+              amount: "1",
+            },
+            { actionType: "Vanquish", icon: "images/vanquish.svg" },
+            { actionType: "Play a Card", icon: "images/play_a_card.svg" },
+          ],
         },
       ],
     },
@@ -88,7 +216,47 @@ export default function Menu({
     //   name: "Ursula",
     //   boardImages: ["images/background_art/Forbidden_Mountain.PNG"],
     // },
-  ];
+  ]);
+
+  const [fadeIn, setFadeIn] = useState(false);
+  const [fadeOutPrev, setFadeOutPrev] = useState(false);
+  const [fadeOutNext, setFadeOutNext] = useState(false);
+
+  const nextProfile = () => {
+    setFadeIn(false);
+    // setFadeOutPrev(false);
+    setFadeOutNext(false);
+    setTimeout(() => {
+      setProfiles((prevItems) => {
+        if (prevItems.length === 0) return prevItems;
+        return [prevItems[prevItems.length - 1], ...prevItems.slice(0, -1)];
+      });
+      setFadeIn(true);
+      // setFadeOutPrev(true);
+      setFadeOutNext(true);
+    }, 100);
+  };
+
+  const prevProfile = () => {
+    setFadeIn(false);
+    setFadeOutPrev(false);
+    // setFadeOutNext(false);
+    setTimeout(() => {
+      setProfiles((prevItems) => {
+        if (prevItems.length === 0) return prevItems;
+        return [...prevItems.slice(1, prevItems.length), prevItems[0]];
+      });
+      setFadeIn(true);
+      setFadeOutPrev(true);
+      // setFadeOutNext(true);
+    }, 100);
+  };
+
+  useEffect(() => {
+    setFadeIn(true);
+    setFadeOutPrev(true);
+    setFadeOutNext(true);
+  }, [profiles[1]]);
 
   return (
     <div className="background">
@@ -109,11 +277,42 @@ export default function Menu({
           )}
           {gameSetUp && (
             <>
-              <h1>Choose your villain:</h1>
+              <div>
+                <button onClick={prevProfile}>Previous</button>
+                <button onClick={nextProfile}>Next</button>
+                <h1>Choose your villain:</h1>
+              </div>
               <div className="profile_container">
-                {profiles.map((profile, index) => (
-                  <Board key={index} profile={profile} />
-                ))}
+                {profiles
+                  .filter((_, index) => index === 0 || index === 2)
+                  .map((profile, index) => {
+                    let animationClass = "";
+
+                    if (index === 0) {
+                      animationClass =
+                        fadeOutPrev ? "fade-out-down" : "";
+                    } else if (index === 2) {
+                      animationClass =
+                        fadeOutNext ? "fade-out-up" : "";
+                    }
+
+                    return (
+                      <Board
+                        key={profile.id}
+                        profile={profile}
+                        className={animationClass}
+                      />
+                    );
+                  })}
+                <div className={`absolute_profile ${fadeIn ? "fade-in" : ""}`}>
+                  {profiles[1] && (
+                    <Board
+                      key={profiles[1].id}
+                      profile={profiles[1]}
+                      className="active"
+                    />
+                  )}
+                </div>
               </div>
             </>
           )}
